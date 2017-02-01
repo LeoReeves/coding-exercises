83deb13ab891726bf0deb9a241fe5771804cb41e
Leo Reeves <leojreeves@gmail.com>
2017-02-01 18:40:33 +0000

    Updated formatting

--- coding-bat/java/string-3.js
+++ coding-bat/java/string-3.js
@@ -5,19 +5,19 @@
  * sensitive). We'll say that a y or z is at the end of a word if there is not
  * an alphabetic letter immediately following it.
  */
- 
+
 function countYZ(str) {
   const strSplit = str.split(' ');
   const arr = [];
   for (let i = 0; i < strSplit.length; i += 1) {
     arr.push(strSplit[i].slice(-1));
   }
-  return arr.length;  
+  return arr.length;
 }
 
-console.log(countYZ("fez day")); // → 2
-console.log(countYZ("day fez")); // → 2
-console.log(countYZ("day fyyyz")); // → 2
+console.log(countYZ('fez day')); // → 2
+console.log(countYZ('day fez')); // → 2
+console.log(countYZ('day fyyyz')); // → 2
 
 /**
  * withoutString
@@ -27,7 +27,7 @@ console.log(countYZ("day fyyyz")); // → 2
  * non-overlapping instances, so with "xxx" removing "xx" leaves "x".
  */
 
-// Solution 1 
+// Solution 1
 function withoutString(base, remove) {
   const regRemove = new RegExp(remove, 'g');
   return base.replace(regRemove, '');
@@ -44,9 +44,9 @@ function withoutString(base, remove) {
   return str;
 }
 
-console.log(withoutString("Hello there", "llo")); // → "He there"
-console.log(withoutString("Hello there", "e")); // → "Hllo thr"
-console.log(withoutString("Hello there", "x")); // → "Hello there"
+console.log(withoutString('Hello there', 'llo')); // → "He there"
+console.log(withoutString('Hello there', 'e')); // → "Hllo thr"
+console.log(withoutString('Hello there', 'x')); // → "Hello there"
 
 /**
  * equalIsNot
@@ -58,7 +58,7 @@ console.log(withoutString("Hello there", "x")); // → "Hello there"
 // Solution 1
 function equalIsNot(str) {
   const strLower = str.toLowerCase();
-  return strLower.split('is').length - 1 === strLower.split('not').length - 1; 
+  return strLower.split('is').length - 1 === strLower.split('not').length - 1;
 }
 
 // Solution 2
@@ -67,6 +67,6 @@ function equalIsNot(str) {
   return strLower.match(/is/g).length === strLower.match(/not/g).length;
 }
 
-console.log(equalIsNot("This is not")); // → false
-console.log(equalIsNot("This is notnot")); // → true
-console.log(equalIsNot("noisxxnotyynotxisi")); // → true
+console.log(equalIsNot('This is not')); // → false
+console.log(equalIsNot('This is notnot')); // → true
+console.log(equalIsNot('noisxxnotyynotxisi')); // → true
